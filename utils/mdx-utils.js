c4dad52661b7aca2c94d40de43fee34eaab82970
Elizabeth Archanjo <eliza.archanjo@gmail.com>
2024-11-03 10:18:07 -0300

    Blog Pessoal com Next.js     Implementando a busca de um POST específico => api.get(`/post?id=eq.${id}`).

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -14,6 +14,17 @@ export const getPostBySlug = async (id) => {
 
     //TODO: BUSCAR UM POST EM ESPECIFICO.
     //const {data} = await api.get(`/post?id=eq.${id}`)
+    try {
+        const { data } = await api.get(`/posts?id=eq.${id}`); // Ajuste aqui para usar 'slug' na query
 
-    return {}
+        if (data && data.length > 0) {
+            return data[0];
+        }
+
+        return null; 
+    } catch (error) {
+        console.error("Erro ao buscar o post:", error);
+        return null;
+    }
 }
+    return {}
